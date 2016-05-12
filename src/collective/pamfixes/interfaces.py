ce3a09dccdafcdea1ed51db8d17267681af48281
Zach Cashero <zach@propertyshelf.com>
2016-05-12 17:59:35 -0500

    Browser layer inherits from layer in plone.app.multilingual

--- src/collective/pamfixes/interfaces.py
+++ src/collective/pamfixes/interfaces.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.app.multilingual.interfaces import IPloneAppMultilingualInstalled
+
+# from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
-class ICollectivePamfixesLayer(IDefaultBrowserLayer):
+class ICollectivePamfixesLayer(IPloneAppMultilingualInstalled):
     """Marker interface that defines a browser layer."""
