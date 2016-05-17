af31ed2cbe77d59e33616c5a039da6bca9745800
Zach Cashero <zach@propertyshelf.com>
2016-05-16 23:16:33 -0500

    Add basic test for viewlet.

---
+++ src/collective/pamfixes/tests/test_viewlet.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Test viewlet of collective.pamfixes."""
+
+# python imports
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# plone imports
+from plone.app.multilingual.browser.setup import SetupMultilingualSite
+
+# local imports
+from collective.pamfixes import testing
+from collective.pamfixes.viewlets import CustomAlternateLanguagesViewlet
+
+
+class TestViewlet(unittest.TestCase):
+    """Validate setup process for ps.diazo.quito."""
+
+    layer = testing.COLLECTIVE_PAMFIXES_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.app = self.layer['app']
+
+        # Setup language root folders
+        setup_tool = SetupMultilingualSite()
+        setup_tool.setupSite(self.portal)
+
+    def test_viewlet_render(self):
+        """Validate that the alternate languages viewlet renders correctly."""
+        viewlet = CustomAlternateLanguagesViewlet(
+            self.portal['en'],
+            self.app.REQUEST,
+            None,
+        )
+        viewlet.update()
+        self.assertTrue(len(viewlet.alternates), 3)
+
+        viewlet = CustomAlternateLanguagesViewlet(
+            self.portal['es'],
+            self.app.REQUEST,
+            None,
+        )
+        viewlet.update()
+        self.assertTrue(len(viewlet.alternates), 3)
+
+        viewlet = CustomAlternateLanguagesViewlet(
+            self.portal['de'],
+            self.app.REQUEST,
+            None,
+        )
+        viewlet.update()
+        self.assertTrue(len(viewlet.alternates), 3)
