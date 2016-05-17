72fcbbc08bddfebc77a1addf11e993e84014ba89
Thomas Massmann <thomas.massmann@it-spir.it>
2016-05-17 19:11:57 +0200

    Pep8.

--- setup.py
+++ setup.py
@@ -15,7 +15,10 @@ long_description = '\n\n'.join([
 setup(
     name='collective.pamfixes',
     version='1.0a1',
-    description="An add-on for Plone that patches the alternate languages viewlet in plone.app.multilingual to fix a bug in the generation of the relative URLs that are used for the rel=\"alternate\" links on multilingual pages.",
+    description='An add-on for Plone that patches the alternate languages '
+                'viewlet in plone.app.multilingual to fix a bug in the '
+                'generation of the relative URLs that are used for the '
+                'rel="alternate" links on multilingual pages.',
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
