869075894b2fb8dfe3c233e74c1eb289befc9549
Zach Cashero <zach@propertyshelf.com>
2016-05-16 23:10:00 -0500

    Add log message for patching the viewlet.

--- src/collective/pamfixes/__init__.py
+++ src/collective/pamfixes/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
+
+import logging
+
+log = logging.getLogger('collective.pamfixes')
+
+log.warning('Patching alternate languages viewlet for plone.app.multilingual')
